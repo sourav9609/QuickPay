04cb2d70cf049b406e0db0a025ead19545fe834e
Sourav Samanta <160819534+sourav9609@users.noreply.github.com>
2024-10-21 17:42:48 +0530

    1st upload project

---
+++ QuickPay/src/main/java/com/example/QuickPay/QuickPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPayApplication.class, args);
+	}
+
+}
